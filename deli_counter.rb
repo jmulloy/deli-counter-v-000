117a7c1f21d2c351d9cdada3021ff3a0271b308f
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-17 22:38:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,7 @@ def line(katz_deli)
    puts "The line is currently empty."
  end
 end
-def take_a_number(other_deli, name)
-  if other_deli.length == 3
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-end
+def take_a_number(katz_deli, name)
+  katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
