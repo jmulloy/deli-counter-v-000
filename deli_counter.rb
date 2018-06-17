7fccf655c28edb33c0ad729e89a3faed360e8466
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-17 22:48:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,15 @@
 katz_deli = []
 
 def line(katz_deli)
+  new_array = []
  if katz_deli.length == 0
    puts "The line is currently empty."
+ else
+   element = "The line is currently:"
+   katz_deli.each_with_index do |name, index|
+    new_array << "#{index+1}. #{name}"
+  end
+  puts element + "#{new_array.join(" ")}"
  end
 end
 
