b0adc8772bc7c4ce0e448c2c1fdd9591bd6ce5bb
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-17 22:20:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,9 @@ def line(katz_deli)
    puts "The line is currently empty."
  end
 end
+def take_a_number(katz_deli, name)
+  if take_a_number == 1
+    puts "Welcome, #{name}. You are number #{index} in line."
+  end
+end
+
